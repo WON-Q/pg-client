fe2c41f49f23b190cff419fe1014d485db24eae0
namsh1125 <namsh1125@naver.com>
2025-04-30 23:58:08 +0900

    아오

--- app/(dashboard)/admin/dashboard/api-keys/page.tsx
+++ app/(dashboard)/admin/dashboard/api-keys/page.tsx
@@ -104,12 +104,12 @@ export default function AdminApiKeysPage() {
 
       <div className="flex flex-col lg:flex-row gap-4 justify-between">
         <div className="flex flex-col sm:flex-row gap-2">
-          <div className="relative w-full max-w-xs">
+          <div className="relative">
             <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-[#5E99D6]" />
             <input
               type="text"
               placeholder="가맹점, API Key 이름 또는 Access Key 검색"
-              className="pl-10 pr-4 py-2 border border-[#CDE5FF] rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#81B9F8]"
+              className="pl-10 pr-4 py-2 border border-[#CDE5FF] rounded-md w-full sm:w-92 focus:outline-none focus:ring-2 focus:ring-[#81B9F8]"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
