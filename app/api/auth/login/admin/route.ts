b627de74ac41b2a74aa94af2abad61f0e7de7cd6
saebomnewspring <sae051@hufs.ac.kr>
2025-05-28 14:07:11 +0900

    Refactor: 로그인 요청/응답 DTO 추출해 리팩토링

--- app/api/auth/login/admin/route.ts
+++ app/api/auth/login/admin/route.ts
@@ -1,46 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { BaseResponse } from "@/types/api";
-import { TokenDto } from "@/types/api";
-
-/**
- * 사용자 로그인 요청 DTO
- */
-export interface LoginRequestDto {
-  /**
-   * 사용자 계정 ID (이메일)
-   */
-  email: string;
-
-  /**
-   * 사용자 비밀번호
-   */
-  password: string;
-}
-
-/**
- * 사용자 로그인 응답 DTO
- */
-export interface LoginResponseDto {
-  /**
-   * 사용자 ID
-   */
-  id: number;
-
-  /**
-   * 사용자 이름
-   */
-  name: string;
-
-  /**
-   * 사용자 이메일
-   */
-  email: string;
-
-  /**
-   * 사용자 토큰 정보
-   */
-  token: TokenDto;
-}
+import { BaseResponse, LoginRequestDto, LoginResponseDto } from "@/types/api";
 
 /**
  * 로그인 API
--- types/api.ts
+++ types/api.ts
@@ -6,6 +6,46 @@ export interface BaseResponse<T> {
 }
 
 /**
+ * 사용자 로그인 요청 DTO
+ */
+export interface LoginRequestDto {
+  /**
+   * 사용자 계정 ID (이메일)
+   */
+  email: string;
+
+  /**
+   * 사용자 비밀번호
+   */
+  password: string;
+}
+
+/**
+ * 사용자 로그인 응답 DTO
+ */
+export interface LoginResponseDto {
+  /**
+   * 사용자 ID
+   */
+  id: number;
+
+  /**
+   * 사용자 이름
+   */
+  name: string;
+
+  /**
+   * 사용자 이메일
+   */
+  email: string;
+
+  /**
+   * 사용자 토큰 정보
+   */
+  token: TokenDto;
+}
+
+/**
  * 토큰 정보 DTO
  */
 export interface TokenDto {
