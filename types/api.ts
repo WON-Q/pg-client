303569438819bf43882617e8149994e24567c6f3
saebomnewspring <sae051@hufs.ac.kr>
2025-05-28 12:28:58 +0900

    Feat: PG사 서버에서 응답받는 토큰 정보 DTO 구현

--- types/api.ts
+++ types/api.ts
@@ -4,3 +4,39 @@ export interface BaseResponse<T> {
   message: string;
   data?: T;
 }
+
+/**
+ * 토큰 정보 DTO
+ */
+export interface TokenDto {
+  /**
+   * 토큰 타입 (예: Bearer)
+   */
+  tokenType: string;
+
+  /**
+   * 사용자 역할
+   */
+  role: "MERCHANT" | "ADMIN";
+
+  /**
+   * 액세스 토큰
+   */
+  accessToken: string;
+
+  /**
+   * 액세스 토큰 만료 시간
+   */
+  accessTokenExpiresAt: string;
+
+  /**
+   * 리프레시 토큰
+   */
+  refreshToken: string;
+
+  /**
+   * 리프레시 토큰 만료 시간
+   */
+  refreshTokenExpiresAt: string;
+
+}
