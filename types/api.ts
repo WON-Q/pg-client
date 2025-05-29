55b4a5f71ba8e4d710afe873ebaf6ffea377d4b9
saebomnewspring <sae051@hufs.ac.kr>
2025-05-29 21:46:19 +0900

    Feat: ApiKey 목록 조회 API의 개별 ApiKey DTO 정의

--- types/api.ts
+++ types/api.ts
@@ -128,3 +128,54 @@ export type TransactionStatus =
  * 지불 방법
  */
 export type PaymentMethod = "APP_CARD" | "WOORI_APP_CARD";
+
+/**
+ * ApiKey 목록 조회 API의 개별 ApiKey DTO
+ */
+export interface ApiKeyResponseDto {
+  /**
+   * API 키 ID
+   */
+  id: number;
+
+  /**
+   * 가맹점 ID
+   */
+  merchantId: number;
+
+  /**
+   * 가맹점 이름
+   */
+  merchantName: string;
+
+  /**
+   * API 키 이름
+   */
+  name: string;
+
+  /**
+   * 액세스 키
+   */
+  accessKey: string;
+
+  /**
+   * 액세스 키 활성화 여부
+   */
+  active: boolean;
+
+  /**
+   * 발급 시각
+   */
+  issuedAt: string;
+
+  /**
+   * 만료 시각
+   */
+  expiresAt: string | null;
+
+  /**
+   * 마지막 사용 시각
+   */
+  lastUsed: string | null;
+
+}
